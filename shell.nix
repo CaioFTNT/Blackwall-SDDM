06360c31056d79b08dddaa8e80e29560908f3613
CaioFTNT <>
2026-07-06 16:43:29 -0300

    feat(nix): add QtMultimedia to shell buildInputs

--- shell.nix
+++ shell.nix
@@ -4,11 +4,12 @@ pkgs.mkShell {
     qt6Packages.sddm
     qt6.qtdeclarative
     qt6.qtshadertools
+    qt6.qtmultimedia
   ];
 
   shellHook = ''
     echo "Shell activated"
 
-    export QML2_IMPORT_PATH="${pkgs.qt6Packages.qtdeclarative}/lib/qt-6/qml:${pkgs.qt6Packages.sddm}/lib/qt-6/qml"
+    export QML2_IMPORT_PATH="${pkgs.qt6.qtdeclarative}/lib/qt-6/qml:${pkgs.qt6.qtmultimedia}/lib/qt-6/qml:${pkgs.qt6Packages.sddm}/lib/qt-6/qml"
   '';
 }
